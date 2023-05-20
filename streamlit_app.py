3edbd6f60ae942b536e72dcb565234fb7168e2f1
Bryan Lambo <63589343+lambobr@users.noreply.github.com>
2023-05-20 22:56:06 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 
 # Let's put a pick list here so they can pick the fruit they want to include
 # streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index)) # the list is the values of index which is values from column "Fruit"
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado','Strawberries']) # adding default picks Avocado and Strawberries
+selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado','Strawberries']) # adding default picks Avocado and Strawberries
 
 # Display the table on the page
-streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(my_fruit_list[my_fruit_list[Fruit].isin(selected)])
